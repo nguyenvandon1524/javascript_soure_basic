fa112303b88cd6ffe4b1e3e2b84e2cc705e08764
nguyenvandon1524 <nguyenvandon152499@gmail.com>
2022-12-14 17:59:10 +0700

    add file javascriptfile2.js and update index.html

---
+++ javascriptfile2.js
@@ -0,0 +1,12 @@
+console.log('Hello world from javascript')
+
+
+// let a = 7;
+// let b = 6;
+// let sinhvien = "NGUYEN VAN DON";
+
+//const x = 5;
+
+let x = 5, y = 6, z = 7;
+k = x * y / z;
+console.log("ket qua ", k)
