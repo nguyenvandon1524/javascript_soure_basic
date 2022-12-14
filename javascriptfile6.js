eb0938d5b4b8509a1f70e16b165c1b159e0036f8
nguyenvandon1524 <nguyenvandon152499@gmail.com>
2022-12-14 18:59:00 +0700

    add file javascriptfile6.js and update index.html

---
+++ javascriptfile6.js
@@ -0,0 +1,8 @@
+console.log('Hello world from HTML')
+let a = 5;;
+let b = 10;
+console.log('Fifteen is '+ ( a + b ) + ' and\nnot ' + (2 * a + b) + '.');
+console.log('Fifteen is '+ a + b + ' and not ' + (2 * a + b) + '.');
+
+
+console.log(`Result: a = ${a} b = ${b} \n a+b = ${a + b}`)
