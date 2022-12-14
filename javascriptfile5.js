88826fa47ee1a8410edb9e48d7b77ff98c5c36b5
nguyenvandon1524 <nguyenvandon152499@gmail.com>
2022-12-14 18:40:24 +0700

    add javascriptfile5.js and update index.html

---
+++ javascriptfile5.js
@@ -0,0 +1,9 @@
+console.log('Hello world from HTML'.toUpperCase())
+let a = 5;
+let b = 20;
+let c = 39.6;
+let d = '5';
+let e = +5;
+let f = Number(d);
+console.log('e value: ', e, 'type: ', typeof e)
+console.log(a === e, 'a: ', typeof a, 'd: ', typeof d)
