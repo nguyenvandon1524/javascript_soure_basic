b0daa861acfdfd7784f8e5475698453b14d8fa0e
nguyenvandon1524 <nguyenvandon152499@gmail.com>
2022-12-15 12:05:55 +0700

    add javascriptfile9.js and update index.html

---
+++ javascriptfile9.js
@@ -0,0 +1,17 @@
+console.log('Hello World From HTML')
+//compare ">", "<", "=", "==", "==="
+let a = 5;
+let b = 6;
+
+console.log('compare a == b: ', 'a = ', a, 'b = ', b, 'Result: ', a == b )  //false
+console.log('compare a <= b: ', 'a = ', a, 'b = ', b, 'Result: ', a <= b )  //true
+console.log('compare a >= b: ', 'a = ', a, 'b = ', b, 'Result: ', a >= b )  //false
+console.log('compare a === b: ', 'a = ', a, 'b = ', b, 'Result: ', a === b ) //false
+
+//Empty, null, undefined
+let c; //Undefined
+console.log('Value c: ', c)
+let d = {}; //Empty
+console.log('Value d: ', d)
+let f = null; //Null
+console.log('Value f: ', f)
