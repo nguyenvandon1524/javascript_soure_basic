73ef2019ab49df327a8c722ba14a8f6d85f99d44
nguyenvandon1524 <nguyenvandon152499@gmail.com>
2022-12-15 11:11:13 +0700

    add file javascriptfile7.j and update index.html

---
+++ javascriptfile7.js
@@ -0,0 +1,16 @@
+console.log('Hello World From HTML')
+
+//key: value
+let a = {
+    name: 'Don',
+    address: 'TPHCM',
+    job: 'IT',
+    email: "nguyenvandon@gmail.com"
+};
+let b = 'Don';
+
+console.log('what is your name? ',' My name is: ', a.name)
+console.log('what is your address? ',' My address is: ', a.address)
+console.log('what is your job? ',' My job is: ', a.job)
+console.log('what is your email? ',' My email is: ', a.email)
+//console.log('type of a: ', typeof a,'type of b: ', typeof b)
