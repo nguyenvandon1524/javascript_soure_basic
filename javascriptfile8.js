abe5ccec8ba345853976d6d8a2688d66543979c9
nguyenvandon1524 <nguyenvandon152499@gmail.com>
2022-12-15 11:41:29 +0700

    add javascriptfile8.js and update index.html

---
+++ javascriptfile8.js
@@ -0,0 +1,20 @@
+console.log('Hello World With Java Script');
+let a = {
+    name: 'Don',
+    age: '23',
+    address: '14/123 Binh Chanh'
+}
+
+console.log('check data object: ', a)
+console.log('my name is: ', a.name);
+
+//Array arr = [item1, item2, item3]
+let b = ['MU', 'Chelsea', 'Everton'];
+console.log('typof b: ', typeof b, b)
+console.log('first item: ', b[0], 'second item:', b[1])
+
+let family = [
+    {name: 'Don', age: 23},
+    {name: 'Dinh', age: 23}
+]
+console.log('My Family: ', family)
