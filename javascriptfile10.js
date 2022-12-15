8ac04d7600d47fa67e740a844a8c36609c586e88
nguyenvandon1524 <nguyenvandon152499@gmail.com>
2022-12-15 16:48:57 +0700

    add file javascriptfile10.js and index.html

---
+++ javascriptfile10.js
@@ -0,0 +1,9 @@
+    console.log('Hello World From HTML')
+
+//for 
+let arr = ['MU', 'Mancity', 'Liverpool', 'Chelsea']
+for(let i = 0; i < arr.length; i++){
+    console.log('check variable i = ', arr[i])
+}
+
+//console.log('check value i: ', i)
