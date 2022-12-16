87b6d7d10e0ed99cd9553e824d47c1d01103d43e
nguyenvandon1524 <nguyenvandon152499@gmail.com>
2022-12-16 11:21:49 +0700

    add file javascriptfile.js and update index.html

---
+++ javascriptfile15.js
@@ -0,0 +1,12 @@
+console.log('Hello World From HTML')
+
+// function getSum(a, b){
+//     return a + b;
+// }
+
+//arrow function 
+let sum = (a,b) => {
+    return a + b;
+}
+
+console.log('Check sum: ', sum(10, 7))
