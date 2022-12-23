cd8fc2d85325b60ad503ca142651fc4b0d291811
nguyenvandon1524 <nguyenvandon152499@gmail.com>
2022-12-23 17:05:48 +0700

    add new file and update js and html

---
+++ javascriptfile21.js
@@ -0,0 +1,25 @@
+console.log('Hello World From Javascript')
+
+let arr = [
+    sv = {
+        name: "Nguyen Van Don",
+        code_studend: "18211TT0287",
+        password: "123456"
+    },
+    sv1 = {
+        name: "Nguyen Thi My Dinh",
+        code_studend: "18211TA5314",
+        password: "123456"
+    },
+    sv3 = {
+        name: "Nguyen Ve Dang",
+        code_studend: "18211TT5323",
+        password: "123456"
+    }
+] 
+
+for (let i = 0; i < arr.length; i++){
+    console.log('Student List: ', arr[i]);
+}
+
+
